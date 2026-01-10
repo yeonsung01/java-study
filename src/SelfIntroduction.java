0f69d9472510ee1dac32a4f1b5aad27d96a28a19
yeonsung <bedro0102@naver.com>
2026-01-10 17:44:17 +0900

    first commit

--- src/SelfIntroduction.java
+++ src/SelfIntroduction.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class SelfIntroduction {
+    public static void main(String[]args) {
+
+        System.out.println("=============================");
+        System.out.println("           자기소개            ");
+        System.out.println("=============================");
+        System.out.println("  이름: 유연성");
+        System.out.println("  나이: 34 살");
+        System.out.println("  취미: 러닝, 사진");
+        System.out.println("=============================");
+
+        System.out.println();
+        System.out.println("올해 목표: ");
+        System.out.println(" 1. Java 문법 완벽 이해");
+        System.out.println(" 2. Spring Framework 학습");
+        System.out.println(" 3. 개인 프로젝트 완성");
+        System.out.println(" 4. 이직 성공하기");
+
+    }
 }
--- src/SimpleCalculation.java
+++ src/SimpleCalculation.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class SimpleCalculation {
+    public static void main(String[] args){
+
+        System.out.println("====================");
+        System.out.println("       간단한 계산     ");
+        System.out.println("====================");
+
+        //숫자 출력
+        System.out.println();
+        System.out.println("===사칙연산 미리보기 ===");
+        System.out.println("10 + 3 = " + (10 + 3));
+        System.out.println("10 - 3 = " + (10 - 3));
+        System.out.println("10 * 3 = " + (10 * 3));
+        System.out.println("10 / 3 = " + (10 / 3));
+        System.out.println("10 % 3 = " + (10 % 3));  //나머지
+
+        //문자열 연결
+        System.out.println();
+        System.out.println("=== 문자열 연결 ===");
+        System.out.println("Hello" + " " +"Java" + "1");
+        System.out.println("1_2 = " + (1+2));
+        System.out.println("결과:" + 100 + "점");
+    }
 }
