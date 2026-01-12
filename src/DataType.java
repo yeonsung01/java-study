5be2c38c20e62e545204aaa16cc2f51296949ab4
yeonsung <bedro0102@naver.com>
2026-01-12 22:22:35 +0900

    Add today's Java practice

---
+++ src/DataType.java
@@ -0,0 +1,33 @@
+public class DataType {
+    public static void main(String[] args) {
+        //정수형
+        byte byteVar = 127;
+        short shortVar = 32767;
+        int intVar = 214748347;
+        long longVar = 9223370236854775807L;
+        System.out.println("=== 정수형 ===");
+        System.out.println("byte: " + byteVar);
+        System.out.println("short: " + shortVar);
+        System.out.println("int: " + intVar);
+        System.out.println("long: " + longVar);
+        //실수형
+        float floatVar = 3.14f;
+        double doubleVar = 3.141592653589793;
+        System.out.println("\n=== 실수형 ===");
+        System.out.println("float: " + floatVar);
+        //문자형
+        char charVar = 'A';
+        char koreanChar = '가';
+        System.out.println("\n=== 문자형 ===");
+        System.out.println("char: " + charVar);
+        System.out.println("한글: " + koreanChar);
+        //논리형
+        boolean isJavaFun = true;
+        boolean isHard = false;
+        System.out.println("\n=== 논리형 ===");
+        System.out.println("isJavaFun: " + isJavaFun);
+        System.out.println("isHard: " + isHard);
+
+
+    }
+}
